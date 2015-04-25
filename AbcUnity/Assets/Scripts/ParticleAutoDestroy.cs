a3061341572d1fbad80f987705921357470aa016
Tommi Laine <ttlain@ttlain.net>
2015-04-25 19:21:50 +0300

    add particle effect to death

---
+++ AbcUnity/Assets/Scripts/ParticleAutoDestroy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ParticleAutoDestroy : MonoBehaviour 
+{
+	private ParticleSystem particleSystem;	
+	public void Start() 
+	{
+		particleSystem = GetComponent<ParticleSystem>();
+	}
+	public void Update() 
+	{
+		if (particleSystem)
+		{
+			if (!particleSystem.IsAlive())
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
