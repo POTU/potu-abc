fc7f39dfc6d8b3a7fc1900f0e4e9b1e5bf14f435
Tommi Laine <ttlain@ttlain.net>
2015-04-25 22:05:30 +0300

    hide pause button when paused

--- AbcUnity/Assets/Scripts/UI/Buttons/PauseToggle.cs
+++ AbcUnity/Assets/Scripts/UI/Buttons/PauseToggle.cs
@@ -1,29 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using Jalomieli.Extensions;
 
 public class PauseToggle : MonoBehaviour {
 
-    GameObject PlayButton;
-    Button UpButton;
-    Button RightButton;
+    private GameObject PlayButton;
+	private Button UpButton;
+	private Button RightButton;
+	private CanvasGroup PauseButtonCanvasGroup;
 
-	// Use this for initialization
 	void Start ()
     {
         PlayButton = transform.parent.Find("PlayButton").gameObject;
         UpButton = transform.parent.Find("UpButton").GetComponent<Button>();
         RightButton = transform.parent.Find("RightButton").GetComponent<Button>();
+		PauseButtonCanvasGroup = transform.parent.Find("PauseButton").GetComponent<CanvasGroup>();
 	}
 	
     public void Pause(bool Pause)
     {
-        if (Pause == true)
+        if (Pause)
         {
             Time.timeScale = 0;
             PlayButton.SetActive(true);
             UpButton.interactable = false;
             RightButton.interactable = false;
+			PauseButtonCanvasGroup.alpha = 0f;
         }
         else
         {
@@ -31,6 +33,7 @@ public class PauseToggle : MonoBehaviour {
             PlayButton.SetActive(false);
             UpButton.interactable = true;
             RightButton.interactable = true;
+			PauseButtonCanvasGroup.alpha = 1f;
         }
     }
 }
