8a69277e81f1ba84ccc85ba9c77e9b7b4a1afc22
unknown <pitkanen.eetu@gmail.com>
2015-04-25 20:52:21 +0300

    Scaling background on x-axis

---
+++ AbcUnity/Assets/Scripts/ElementScaler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ElementScaler : MonoBehaviour {
+
+	float ReferenceHeight;
+	float CurrentHeight;
+	float HeightRatio;
+	
+	float ReferenceWidth;
+	float CurrentWidth;
+	float WidthRatio;
+
+	// Use this for initialization
+	void Start ()
+	{
+        CurrentHeight = Screen.height;		
+        CurrentWidth = Screen.width;
+
+        float CurrentRatio = CurrentWidth / CurrentHeight;
+
+        print(CurrentRatio);
+
+        float ReferenceRatio = 2048f / 1536f;
+
+        print(ReferenceRatio);
+
+        float DifferenceRatio = CurrentRatio / ReferenceRatio;
+
+        transform.localScale = new Vector2(transform.localScale.x * DifferenceRatio, transform.localScale.y);
+        print(DifferenceRatio);
+    }
+}
