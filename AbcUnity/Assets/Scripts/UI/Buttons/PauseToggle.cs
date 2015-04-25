f88363235dc5e6d707878c1db968e4f2997120d6
unknown <pitkanen.eetu@gmail.com>
2015-04-25 19:44:51 +0300

    Pause functionality, gameplay background

---
+++ AbcUnity/Assets/Scripts/UI/Buttons/PauseToggle.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseToggle : MonoBehaviour {
+
+    GameObject PlayButton;
+    Button UpButton;
+    Button RightButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PlayButton = transform.parent.Find("PlayButton").gameObject;
+        UpButton = transform.parent.Find("UpButton").GetComponent<Button>();
+        RightButton = transform.parent.Find("RightButton").GetComponent<Button>();
+	}
+	
+    public void Pause(bool Pause)
+    {
+        if (Pause == true)
+        {
+            Time.timeScale = 0;
+            PlayButton.SetActive(true);
+            UpButton.interactable = false;
+            RightButton.interactable = false;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            PlayButton.SetActive(false);
+            UpButton.interactable = true;
+            RightButton.interactable = true;
+        }
+    }
+}
