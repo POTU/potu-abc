2c204f5a4258fcef5311bc0a8ace061f683c085f
Tommi Laine <ttlain@ttlain.net>
2015-04-25 14:01:25 +0300

    bind move buttons

---
+++ AbcUnity/Assets/Scripts/UI/Buttons/RightButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Jalomieli.Extensions;
+
+public class RightButton : MonoBehaviour 
+{
+	public void PlayerRightAction()
+	{
+		var player = GameObject.FindGameObjectWithTag("Player");
+		player.DemandComponent<PlayerController>().MoveRightAction();
+	}
+}
---
+++ AbcUnity/Assets/Scripts/UI/Buttons/UpButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Jalomieli.Extensions;
+
+public class UpButton : MonoBehaviour 
+{
+	public void PlayerUpAction()
+	{
+		var player = GameObject.FindGameObjectWithTag("Player");
+		player.DemandComponent<PlayerController>().MoveUpAction();
+	}
+}
