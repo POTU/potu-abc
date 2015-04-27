03847a1abe55b11af7827925b978798de9be929a
Tommi Laine <me@ruk.si>
2015-04-27 14:35:53 +0300

    add quit button to pause screen

--- AbcUnity/Assets/Scripts/UI/Buttons/PauseToggle.cs
+++ AbcUnity/Assets/Scripts/UI/Buttons/PauseToggle.cs
@@ -5,17 +5,19 @@ using Jalomieli.Extensions;
 public class PauseToggle : MonoBehaviour {
 
     private GameObject PlayButton;
+    private GameObject QuitButton;
 	private Button UpButton;
 	private Button RightButton;
 	private CanvasGroup PauseButtonCanvasGroup;
 
-	void Start ()
+    void Awake()
     {
         PlayButton = transform.parent.Find("PlayButton").gameObject;
+        QuitButton = transform.parent.Find("QuitButton").gameObject;
         UpButton = transform.parent.Find("UpButton").GetComponent<Button>();
         RightButton = transform.parent.Find("RightButton").GetComponent<Button>();
-		PauseButtonCanvasGroup = transform.parent.Find("PauseButton").GetComponent<CanvasGroup>();
-	}
+        PauseButtonCanvasGroup = transform.parent.Find("PauseButton").GetComponent<CanvasGroup>();
+    }
 	
     public void Pause(bool Pause)
     {
@@ -24,6 +26,7 @@ public class PauseToggle : MonoBehaviour {
         {
             Time.timeScale = 0;
             PlayButton.SetActive(true);
+            QuitButton.SetActive(true);
             UpButton.interactable = false;
             RightButton.interactable = false;
 			PauseButtonCanvasGroup.alpha = 0f;
@@ -32,9 +35,23 @@ public class PauseToggle : MonoBehaviour {
         {
             Time.timeScale = 1;
             PlayButton.SetActive(false);
+            QuitButton.SetActive(false);
             UpButton.interactable = true;
             RightButton.interactable = true;
 			PauseButtonCanvasGroup.alpha = 1f;
         }
+    }
+
+    public void PauseToQuit()
+    {
+        // This shouldn't be here but I'm too lazy - Ruksi
+        Time.timeScale = 1;
+        PlayButton.SetActive(false);
+        QuitButton.SetActive(false);
+        UpButton.interactable = true;
+        RightButton.interactable = true;
+        PauseButtonCanvasGroup.alpha = 1f;
+        AudioSystem.Get().PlayUI();
+        Application.LoadLevel("StartScreen");
     }
 }
